7699fd52377d98ed7b6183bfb164d09895d50a82
Anastasiya Shamilova <nastyaespano22@yandex.ru>
2019-03-11 21:45:41 +0300

    added 2 and 1 task

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -23,10 +23,21 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №2 писать здесь
-    //console.log(result)
-    //return result;
+let today = new Date().getFullYear();
+    let old = new Date().getFullYear() - dateOfBirthday;
+    if (old >= 18) {
+      console.log(`Не желаете ли олд-фэшн, ${name}?`)
+    };
+  
+    if (old < 18) {
+      console.log(`Сожалею, ${name} , но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+    };
+   
+    return old;
+    
 }
+askDrink("Pavel" , 1997)    
+
 
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number);
@@ -35,6 +46,19 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
-    // код для задачи №3 писать здесь
-    //return averageMark;
+   let grade = [2, 3, 4, 5, 5];
+   let total = 0;
+   
+   if (grade.length > 5) {
+       console.log(grade.splice(5))
+       console.log((grade[0] + grade[1] + grade[2] + grade[3]+ grade[4])/grade.length);
+   }
+   if (grade.length <= 5) {
+     for(let i in grade) {total += grade[i];}
+    console.log(total/grade.length);
+   }
+   return grade;
+  
 }
+
+ getAverageMark()
